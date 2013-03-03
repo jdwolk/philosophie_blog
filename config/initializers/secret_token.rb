d44115f7a0d99856c52d5909882710837419b7fb
Jordaniel Wolk <jdwolk@gmail.com>
2013-03-03 12:49:06 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhilosophieBlog::Application.config.secret_token = 'cdd422d91bb074adc9bea627cb57d596e6c30ae17ddd336d90e8757dbf10a84af69930cd4c170714412063bd0ba21cd0b5391301ed989a9c48bb055472e3f556'
