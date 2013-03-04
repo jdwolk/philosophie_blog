29eb1b06c3426f3fd0fd841535e2ac99ca947df2
Jordaniel Wolk <jdwolk@gmail.com>
2013-03-03 16:58:38 -0800

    Added PostTransitions module and initial tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,36 @@
+module PostTransitions
+  extend self  # Allows for private in-module methods
+
+  attr_accessor :transitions, :states, :actions
+
+  self.transitions = {:draft     => {:save       => :draft,
+                                     :done       => :completed,
+                                     :trash      => :tossed},
+                      :completed => {:publish    => :published,
+                                     :edit       => :draft,
+                                     :trash      => :tossed},
+                      :published => {:unpublish  => :completed,
+                                     :trash      => :tossed},
+                      :tossed    => {:restore    => :draft,
+                                     :trash      => :tossed}}
+  self.states = transitions.keys
+  self.actions = [:save, :done, :trash, :publish, :unpublish, :edit, :restore]
+
+  def good_state?(state)
+    self.states.member? state
+  end
+
+  def good_action?(action)
+    self.actions.member? action
+  end
+
+  def transition(state, action)
+    self.transitions[state][action]
+  end
+  
+end
+
 class Post < ActiveRecord::Base
   attr_accessible :body, :published_at, :slug, :state, :title
+
 end
--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
+include PostTransitions
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_all_states_gives_tossed_for_trash_action
+    expected = :tossed
+    PostTransitions.states.each do |s|
+      assert_equal(expected, PostTransitions.transition(s, :trash))
+    end
+  end
+
+  test "in draft, save action" do
+    expected = :draft
+    actual   = PostTransitions.transition(:draft, :save)
+    assert_equal expected, actual
+  end
+
+  test "in draft, done action" do
+    expected = :completed
+    actual   = PostTransitions.transition(:draft, :done)
+    assert_equal expected, actual
+  end
+
+  test "in completed, publish action" do
+    expected = :published
+    actual   = PostTransitions.transition(:completed, :publish)
+    assert_equal expected, actual
+  end
+
+  test "in completed, edit action" do
+    expected = :draft
+    actual   = PostTransitions.transition(:completed, :edit)
+    assert_equal expected, actual
+  end
+
 end
