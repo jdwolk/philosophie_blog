62b216772ea7ce7607ad6c8e89522fabbe21d3f4
Jordaniel Wolk <jdwolk@gmail.com>
2013-03-03 20:57:28 -0800

    Added redcarpet gem for markdown support

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ end
 
 gem 'jquery-rails'
 gem 'friendly_id'
+gem 'redcarpet'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+  def markdown(text)
+    markdown_help(text)
+  end
+
+  def markdown_no_html(text)
+    markdown_help(text, :filter_html)
+  end
+
+  def markdown_help(text, *new_options)
+    options = {:hard_wrap         => true,
+               :autolink          => true,
+               :filter_html       => true,
+               :no_intraemphasis  => true,
+               :fenced_code       => true,
+               :gh_blockcode      => true}
+    
+    renderer = Redcarpet::Render::HTML.new
+    redcarpet = Redcarpet::Markdown.new(renderer, options)
+    raw(redcarpet.render(text))
+  end
 end
